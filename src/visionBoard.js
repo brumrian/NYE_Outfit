95535beee55a77f3420be231521ff9022a9a897f
brumrian <u1378044@umail.utah.edu>
2021-12-23 22:08:09 -0700

    fixed links

--- src/visionBoard.js
+++ src/visionBoard.js
@@ -11,7 +11,7 @@ let subtitleText = document.createTextNode("style by Princess Polly");
 subtitle.appendChild(subtitleText);
 body.appendChild(subtitle);
 let earrings = document.createElement('img');
-earrings.src = "NYE/unnamed-5.jpg";
+earrings.src = "brumrian.github.io/NYE/unnamed-5.jpg";
 earrings.id = "earring";
 earrings.className = "earring";
 earrings.style.height = "273px";
@@ -21,7 +21,7 @@ earrings.addEventListener("mouseover", handleEarringEnter);
 
 
 let earringDiv = document.createElement("img");
-earringDiv.src = "NYE/earrings.png";
+earringDiv.src = "brumrian.github.io/NYE/earrings.png";
 earringDiv.style.height = "273px";
 earringDiv.style.width = "202px";
 earringDiv.id = "earringDiv";
@@ -43,7 +43,7 @@ earringDiv.addEventListener("mouseleave", handleEarringLeave);
 
 
 let necklace = document.createElement('img');
-necklace.src = "NYE/unnamed-6.jpg";
+necklace.src = "brumrian.github.io/NYE/unnamed-6.jpg";
 necklace.id = "necklace";
 necklace.className = "necklace";
 necklace.style.height = "364px";
@@ -51,7 +51,7 @@ necklace.style.width = "269px";
 // necklace.addEventListener("click", earringsLink);
 necklace.addEventListener("mouseover", handleNecklaceEnter);
 let necklaceDiv = document.createElement("img");
-necklaceDiv.src = "NYE/necklace.png";
+necklaceDiv.src = "brumrian.github.io/NYE/necklace.png";
 necklaceDiv.style.height = "364px";
 necklaceDiv.style.width = "269px";
 necklaceDiv.id = "necklaceDiv";
@@ -61,14 +61,14 @@ necklaceDiv.addEventListener("mouseleave", handleNecklaceLeave);
 
 
 let dress = document.createElement('img');
-dress.src = "NYE/unnamed.jpg";
+dress.src = "brumrian.github.io/NYE/unnamed.jpg";
 dress.id = "dress";
 dress.className = "dress";
 dress.style.height = "728px";
 dress.style.width = "538px";
 dress.addEventListener("mouseover", handleDressEnter);
 let dressDiv = document.createElement("img");
-dressDiv.src = "NYE/dress.png";
+dressDiv.src = "brumrian.github.io/NYE/dress.png";
 dressDiv.style.height = "728px";
 dressDiv.style.width = "538px";
 dressDiv.id = "dressDiv";
@@ -80,13 +80,13 @@ dressDiv.addEventListener("mouseleave", handleDressLeave);
 dress.addEventListener("mouseover", handleDressEnter);
 
 let purse = document.createElement('img');
-purse.src = "NYE/unnamed-3.jpg";
+purse.src = "brumrian.github.io/NYE/unnamed-3.jpg";
 purse.id = "bag";
 purse.style.height = "546px";
 purse.style.width = "404px";
 purse.addEventListener("mouseover", handlePurseEnter);
 let purseDiv = document.createElement("img");
-purseDiv.src = "NYE/bag.png";
+purseDiv.src = "brumrian.github.io/NYE/bag.png";
 purseDiv.style.height = "546px";
 purseDiv.style.width = "404px";
 purseDiv.id = "purseDiv";
@@ -96,13 +96,13 @@ purseDiv.addEventListener("mouseleave", handlePurseLeave);
 
 
 let boots = document.createElement('img');
-boots.src = "NYE/unnamed-2.jpg";
+boots.src = "brumrian.github.io/NYE/unnamed-2.jpg";
 boots.id = "boots";
 boots.style.height = "637px";
 boots.style.width = "471px";
 boots.addEventListener("mouseover", handleBootsEnter);
 let bootsDiv = document.createElement("img");
-bootsDiv.src = "NYE/boots.png";
+bootsDiv.src = "brumrian.github.io/NYE/boots.png";
 bootsDiv.style.height = "637px";
 bootsDiv.style.width = "471px";
 bootsDiv.id = "bootsDiv";
